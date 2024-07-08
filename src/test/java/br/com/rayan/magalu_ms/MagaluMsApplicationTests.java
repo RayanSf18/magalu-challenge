d208e2357be68b625956504f35e3627e054b77f5
rayan <rayansf18@gmail.com>
2024-07-08 14:02:50 -0300

    feat: project created.

---
+++ src/test/java/br/com/rayan/magalu_ms/MagaluMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rayan.magalu_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
